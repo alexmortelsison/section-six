fe49c56043770c5b7d3bc380e5fb54ad4a9fb349
alexmortelsison <alexmortelsison@gmail.com>
2024-07-30 21:29:56 +0900

    updated main

---
+++ TDArrays.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class TDArrays {
+  public static void main(String[] args) {
+    int[][] grades = new int[3][4];
+    System.out.println("\t Harry: " + Arrays.toString(grades[0]));
+    System.out.println("\t Ron: " + Arrays.toString(grades[1]));
+    System.out.println("\t Hermione: " + Arrays.toString(grades[2]));
+  }
+}
